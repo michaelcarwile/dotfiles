afea0d365bf1f422def8aa9d63edde5b3908e9b9
Michael Carwile <michael@carwile.me>
2016-07-10 13:14:39 -0500

    Add manual install of Vundle

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -7,5 +7,11 @@ echo "Installing to $HOME"
 echo "Symlink (.)dotfiles"
 ln -s dotfiles/.* $HOME
 
+# manually install Vundle for now
+cd dotfiles/.vim/bundle/Vundle.vim
+git init
+git remote add remote https://github.com/VundleVim/Vundle.vim.git
+git pull remote master
+
 echo "Install Vundle plugins"
 vim +PluginInstall +qall
